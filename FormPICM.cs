ce5ff3371926bc59771a1152e23fa0386dc5e626
John Hilasaca <89051327+Jaalya@users.noreply.github.com>
2022-11-22 08:46:34 -0500

    Add Form

---
+++ FormPICM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCTS
+{
+    public partial class FormPICM : Form
+    {
+        public FormPICM()
+        {
+            InitializeComponent();
+        }
+    }
+}
