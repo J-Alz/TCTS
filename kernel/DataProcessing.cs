57d6c2dff242eeb0d2812f525f42b4d74aca653a
John Hilasaca <89051327+Jaalya@users.noreply.github.com>
2022-12-13 23:49:43 -0500

    se creo el nucleo para procesar fracciones

---
+++ kernel/DataProcessing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCTS.fractionKernel;
+
+namespace TCTS.kernel
+{
+    internal class DataProcessing
+    {
+        public static fraction operator +(fraction a,fraction b)
+        {
+
+        }
+    }
+}
